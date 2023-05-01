0b0e637bbf5943917f8abecd2963ac8dc35cb1db
wxt669698 <1970550145@qq.com>
2023-05-01 19:23:28 +0800

    更新官方跨链桥逻辑

--- src/utils/getZkSyncBridge.js
+++ src/utils/getZkSyncBridge.js
@@ -1,28 +1,75 @@
 import axios from "axios";
 import {ethers} from "ethers";
 
+async function processTransactions(
+    list,
+    l1Tol2Times,
+    l1Tol2Amount,
+    l2Tol1Times,
+    l2Tol1Amount
+) {
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].isL1Originated === true) {
+            l1Tol2Times++;
+            const value = ethers.formatEther(list[i].data.value, "ether");
+            l1Tol2Amount += parseFloat(value);
+        } else if (
+            list[i].data.contractAddress ===
+            "0x000000000000000000000000000000000000800a"
+        ) {
+            l2Tol1Times++;
+            const value = ethers.formatEther(list[i].data.value, "ether");
+            l2Tol1Amount += parseFloat(value);
+        }
+    }
+    return [l1Tol2Times, l1Tol2Amount, l2Tol1Times, l2Tol1Amount];
+}
+
 async function getZkSyncBridge(address) {
-    let url =
-        "https://zksync2-mainnet-explorer.zksync.io/transactions?limit=100&direction=older&accountAddress=" +
-        address;
     try {
-        const response = await axios.get(url);
         let l1Tol2Times = 0;
         let l1Tol2Amount = 0;
         let l2Tol1Times = 0;
         let l2Tol1Amount = 0;
-        for (let i = 0; i < response.data.list.length; i++) {
-            if (response.data.list[i].isL1Originated === true) {
-                l1Tol2Times++;
-                const value = ethers.formatEther(response.data.list[i].data.value,);
-                l1Tol2Amount += parseFloat(value);
-            } else if (
-                response.data.list[i].data.contractAddress ===
-                "0x000000000000000000000000000000000000800a"
-            ) {
-                l2Tol1Times++;
-                const value = ethers.formatEther(response.data.list[i].data.value,);
-                l2Tol1Amount += parseFloat(value);
+        let offset = 0;
+        let fromBlockNumber = null;
+        let fromTxIndex = null;
+        const initUrl = "https://zksync2-mainnet-explorer.zksync.io/transactions?limit=100&direction=older&accountAddress=" + address;
+        const initResponse = await axios.get(initUrl)
+        const initDataLength = initResponse.data.total;
+        [l1Tol2Times, l1Tol2Amount, l2Tol1Times, l2Tol1Amount] =
+            await processTransactions(
+                initResponse.data.list,
+                l1Tol2Times,
+                l1Tol2Amount,
+                l2Tol1Times,
+                l2Tol1Amount
+            );
+        if (initDataLength > 100) {
+            fromBlockNumber = initResponse.data.list[0].blockNumber;
+            fromTxIndex = initResponse.data.list[0].indexInBlock;
+            while (true) {
+                let url = `https://zksync2-mainnet-explorer.zksync.io/transactions?limit=100&direction=older&accountAddress=${address}`;
+                if (fromBlockNumber !== undefined && fromTxIndex !== undefined && offset !== 0) {
+                    url += `&fromBlockNumber=${fromBlockNumber}&fromTxIndex=${fromTxIndex}&offset=${offset}`;
+                }
+                const response = await axios.get(url);
+                const ListLength = response.data.list.length;
+                [l1Tol2Times, l1Tol2Amount, l2Tol1Times, l2Tol1Amount] =
+                    await processTransactions(
+                        response.data.list,
+                        l1Tol2Times,
+                        l1Tol2Amount,
+                        l2Tol1Times,
+                        l2Tol1Amount
+                    );
+                if (ListLength === 100) {
+                    fromBlockNumber = response.data.list[0].blockNumber;
+                    fromTxIndex = response.data.list[0].indexInBlock;
+                    offset += ListLength;
+                } else {
+                    break;
+                }
             }
         }
         return {
@@ -32,7 +79,7 @@ async function getZkSyncBridge(address) {
             l2Tol1Amount: l2Tol1Amount.toFixed(3)
         }
     } catch (e) {
-        console.log(e)
+        console.log(e);
         return {l1Tol2Times: "Error", l1Tol2Amount: "Error", l2Tol1Times: "Error", l2Tol1Amount: "Error"}
     }
 }
